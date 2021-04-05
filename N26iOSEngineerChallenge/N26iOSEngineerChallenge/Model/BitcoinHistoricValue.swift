d61c22ad3deeac0c6556845aac086b79e3d0f0cd
Pol <polpica@hotmail.com>
2021-04-05 13:57:30 +0200

    Delete unecessary code

--- N26iOSEngineerChallenge/N26iOSEngineerChallenge/Model/BitcoinHistoricValue.swift
+++ N26iOSEngineerChallenge/N26iOSEngineerChallenge/Model/BitcoinHistoricValue.swift
@@ -10,21 +10,5 @@ import Foundation
 // MARK: - Empty
 struct BitcoinHistoricValue: Codable {
     var bpi: [String: Double]
-    
-    func getDayFor(indexPath: IndexPath) -> String {
-        return bpi.keys[getIndex(index: indexPath.row)]
-    }
-    
-    func getValueFor(indexPath: IndexPath) -> String {
-        return getFormatedValue(value: bpi.values[getIndex(index: indexPath.row)])
-    }
-    
-    private func getIndex(index: Int) -> Dictionary<String, Double>.Index {
-        return bpi.index(bpi.startIndex, offsetBy: index)
-    }
-    
-    private func getFormatedValue(value: Double) -> String {
-        return String(format: "%.2f", value) + Literals.CurrencySymbol.eur
-    }
 }
 
